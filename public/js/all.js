f23357b561bd660cc211c2a0f9ed2180725434b5
rezafitriaman <rezafitriaman@gmail.com>
2017-08-07 08:49:02 +0200

    page finish

--- public/js/all.js
+++ public/js/all.js
@@ -46,6 +46,69 @@ $(document).ready(function(){
 
     /*================================= END ===============================*/
 
+    /*smothscroll*/
+    // Select all links with hashes
+    $('a[href*="#"]')
+      // Remove links that don't actually link to anything
+      .not('[href="#"]')
+      .not('[href="#0"]')
+      .click(function(event) {
+        // On-page links
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+          && 
+          location.hostname == this.hostname
+        ) {
+          // Figure out element to scroll to
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top - 50
+            }, 1000, function() {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              };
+            });
+          }
+        }
+      });
+
+      /*================================= END ===============================*/
+      /*fix onze bedrijfscultuur block on mobile*/
+    function checkModMediaQuery(){
+
+       // media query event handler
+        if (matchMedia) {
+          const mq = window.matchMedia("(min-width: 992px)");
+          mq.addListener(WidthChange);
+          WidthChange(mq);
+        }
+
+        // media query change
+        function WidthChange(mq) {
+          if (mq.matches) {
+            // window width is at least 992px
+            $('.jq-xs-top').insertAfter($('.jq-xs-bottom'));
+          } else {
+            // window width is less than 992px
+            $('.jq-xs-bottom').insertAfter($('.jq-xs-top'));
+          }
+
+        }
+    }
+    checkModMediaQuery();
+
 });
 
  /*================================= oudside document ready (initMap) ===============================*/
